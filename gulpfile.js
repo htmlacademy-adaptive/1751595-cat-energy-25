41ce4187793b2810a35466729849b22f3a1f6b24
imaks6991 <maksi9664@gmail.com>
2022-05-20 23:05:58 +0500

    Автоматизация сборки личного проекта. Подготовка

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import svgo from 'gulp-svgo';
+import svgstore from 'gulp-svgstore';
+import squoosh from 'gulp-squoosh';
+import rename from 'gulp-rename';
+import csso from 'gulp-csso';
+import del from 'del';
 
 // Styles
 
@@ -14,17 +20,68 @@ export const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+export const minifyCSS = () => {
+  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss( [ autoprefixer() ] ))
+  .pipe(csso())
+  // .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+}
+
+// copy files
+export const copyFiles = () => {
+  return gulp.src([
+  'source/fonts/*.{woff2,woff}',
+  'source/*.ico',
+  'source/*.html',
+  'source/js/*.js'], {base: 'source'}) // {base: 'source'} - to use directories structure
+  .pipe(gulp.dest('build'))
+}
+
+export const copyImages = () => {
+  return gulp.src('source/img/*.{png,jpg}')
+  .pipe(gulp.dest('build/img'))
+}
+
+export const sprite = () => {
+  return gulp.src('source/img/icons/*.svg')
+  .pipe(svgo())
+  .pipe(svgstore({inline: true}))
+  .pipe(gulp.dest('build/img'))
+}
+
+// optimise
+
+export const svg = () => {
+  return gulp.src('source/img/**/*.svg', {base: 'source'})
+  .pipe(svgo())
+  .pipe(gulp.dest('build'))
+}
+
+export const optimiseImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'))
+}
+
+
+// clean
+
+const clean = () => {
+  return del('build');
+  };
 
 // Server
 
 const server = (done) => {
   browser.init({
-    server: {
-      baseDir: 'source'
-    },
+    server: {baseDir: 'build'}, // baseDir means directory browser shows
     cors: true,
     notify: false,
     ui: false,
@@ -38,6 +95,15 @@ const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
+
+
+export const build = gulp.series(
+  clean, copyFiles, optimiseImages,
+  gulp.parallel(
+  minifyCSS, svg,
+  ),
+  sprite,
+);
 
 
 export default gulp.series(
